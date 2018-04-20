9602b3221d30917b8c08102a08d7ae1cca7c9708
chicks427 <chicks427@live.co.uk>
2018-04-20 19:38:19 +0100

    Wrapped code into functions, put both album & song code into single function. Code currently generates linklist for the first album only. Will work on putting this information into a csv and/or pandas df tomorrow, and will then make it work with all the albums.

--- kanye.py
+++ kanye.py
@@ -1,15 +1,30 @@
 from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup as soup
 
-my_url = 'https://genius.com/artists/Kanye-west'
-req = Request(my_url, headers={'User-Agent': 'Mozilla/5.0'})
+def get_html(link):
+	req = Request(link, headers={'User-Agent': 'Mozilla/5.0'})
 
-web_byte = urlopen(req).read()
-page_html = web_byte.decode('utf-8')
+	web_byte = urlopen(req).read()
+	page_html = web_byte.decode('utf-8')
 
-page_soup = soup(page_html, "html.parser") # Parses the html.
+	page_soup = soup(page_html, "html.parser") # Parses the html.
+	return page_soup
 
-albums = page_soup.findAll("a", {"class": "vertical_album_card"})
+def get_links(link_type, soup):
+	link_list = []
+	if link_type == "album":
+		links = soup.findAll("a", {"class": "vertical_album_card"})
+	elif link_type == "song":
+		links = soup.findAll("a", {"class": "u-display_block"})
+	for link in links:
+		link_list.append(link["href"])
+	return link_list
 
-for album in albums:
-	print(album["href"])
+Kanye_soup = get_html('https://genius.com/artists/Kanye-west')
+Kanye_albums = get_links('album', Kanye_soup)
+
+pablo = get_html(Kanye_albums[0])
+kanye_tracklist = get_links('song', pablo)
+
+for song in kanye_tracklist:
+	print(song)
